14f5e639ed7d94a0111c4bbaa68e40be950fca8b
kiplangatmike <m.kiplangat@alustudent.com>
2023-04-09 01:30:48 +0200

    feat: updates

--- src/pages/messaging/index.tsx
+++ src/pages/messaging/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import Layout from "$components/Layout";
+import Image from "next/image";
 
 const ChatInterface = () => {
   return (
@@ -11,14 +12,14 @@ const ChatInterface = () => {
             <div className="mr-4 max-w-xs break-words rounded-lg bg-black px-4 py-2 text-sm shadow-md">
               Hello Liplan?
             </div>
-            <img
+            <Image
               src="https://via.placeholder.com/150"
               className="h-10 w-10 rounded-full"
               alt="Avatar"
             />
           </div>
           <div className="mt-4 flex items-start justify-start pl-2">
-            <img
+            <Image
               src="https://via.placeholder.com/150"
               className="h-10 rounded-full"
               alt="Avatar"
